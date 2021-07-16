0c9d61d5558bf60f869c0c19938b14321357c138
datae <accounts@datae.de>
2021-07-16 18:01:27 +0200

    bugfix failing belongs to spec

--- spec/dummy/app/models/topic.rb
+++ spec/dummy/app/models/topic.rb
@@ -1,6 +1,6 @@
 class Topic < ApplicationRecord
   belongs_to :user
-  belongs_to :creator, class_name: 'User', optional: true
+  belongs_to :creator, class_name: 'User', optional: true, foreign_key: 'creator_id'
 
   acts_as_association_scope
 end
