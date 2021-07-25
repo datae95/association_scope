a9695dd63ec27aa9cbaf8ad7d4ef858a47f0fcf8
Daniel Täsch <accounts@datae.de>
2021-07-25 16:48:18 +0200

    fix belongs to reflection

--- lib/association_scope/scope/belongs_to_reflection.rb
+++ lib/association_scope/scope/belongs_to_reflection.rb
@@ -5,15 +5,20 @@ module AssociationScope
         association = @association
         details = model.reflections[association]
         class_name = details.options[:class_name]&.constantize || association.camelize.constantize
-        table_name = model.to_s.underscore.pluralize.to_sym
         association_name = association.to_s.underscore.to_sym
         foreign_key = details.options[:foreign_key]
         own_table_name = class_name.to_s.pluralize.underscore
 
+        new_reflection = class_name.reflections[model.to_s.underscore.singularize] || class_name.reflections[model.to_s.underscore.pluralize]
+        table_name = if new_reflection.source_reflection.class.to_s.split("::").last == "HasOneReflection"
+          model.to_s.underscore.to_sym
+        else
+          model.to_s.underscore.pluralize.to_sym
+        end
+
         model.class_eval <<-RUBY, __FILE__, __LINE__ + 1
           scope association.pluralize, -> do
             if foreign_key.present?
-
               class_name
                 .joins("JOIN #{table_name} ON #{table_name}.#{foreign_key} = #{own_table_name}.id")
             else
--- spec/belongs_to_reflection_spec.rb
+++ spec/belongs_to_reflection_spec.rb
@@ -2,20 +2,33 @@ RSpec.describe "AssociationScope::BelongsToReflection" do
   let!(:user1) { User.create! }
   let!(:user2) { User.create! }
   let!(:user3) { User.create! }
+  let!(:user4) { User.create! }
 
   let!(:topic1) { Topic.create!(user: user1, creator: user2) }
   let!(:topic2) { Topic.create!(user: user2, creator: user3) }
   let!(:topic3) { Topic.create!(user: user2, creator: nil) }
 
-  context "with standard association" do
-    it { expect(Topic.all.users).to match_array [user1, user2] }
+  let!(:account1) { Account.create!(user: user1) }
+  let!(:account2) { Account.create!(user: user2) }
+  let!(:account4) { Account.create!(user: user4) }
+
+  context 'with has many association' do
+    context "with standard association" do
+      it { expect(Topic.users).to match_array [user1, user2] }
+    end
+
+    context "with named association" do
+      it { expect(Topic.creators).to match_array [user2, user3] }
+    end
+
+    context "when optional" do
+      it { expect(Topic.where(id: topic3.id).creators).to eq [] }
+    end
   end
 
-  context "with other named association" do
-    it { expect(Topic.all.creators).to match_array [user2, user3] }
-  end
-
-  context "when optional" do
-    it { expect(Topic.where(id: topic3.id).creators).to eq [] }
+  context 'with has one association' do
+    context "with standard association" do
+      it { expect(Account.users).to match_array [user1, user2, user4] }
+    end
   end
 end
