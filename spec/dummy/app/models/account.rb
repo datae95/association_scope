6221f04318e964a9fccb7c2f708e63377526451e
datae <accounts@datae.de>
2021-08-01 13:29:18 +0200

    don't build scopes, not listed in only

--- spec/dummy/app/models/account.rb
+++ spec/dummy/app/models/account.rb
@@ -2,7 +2,8 @@
 
 class Account < ApplicationRecord
   belongs_to :user
+  belongs_to :owner, class_name: "User", foreign_key: :user_id
   has_many :topics, through: :user
 
-  acts_as_association_scope
+  acts_as_association_scope only: [:user, :topics]
 end
--- spec/except_spec.rb
+++ spec/except_spec.rb
@@ -13,4 +13,7 @@ RSpec.describe "except rules" do
   context "when reflection is listed in except" do
     it { expect{ Topic.owners }.to raise_error(NoMethodError) }
   end
-end
+
+  context "when reflection is not litsed in only" do
+    it { expect{ Account.owners }.to raise_error(NoMethodError) }
+  end
